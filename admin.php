b0225bca746f8e098616db946671d99e1727069d
Theseadev <Fahrulbahri0520@gmail.com>
2026-09-05 12:00:17 +0800

    Fix duplicate plus sign on topbar add button

--- admin.php
+++ admin.php
@@ -1185,7 +1185,7 @@ $sidebarCommit = substr($versionData['current_commit'] ?? '5540ac8', 0, 7);
                             <line x1="12" y1="5" x2="12" y2="19"></line>
                             <line x1="5" y1="12" x2="19" y2="12"></line>
                         </svg>
-                        <span>+ Tambah Template</span>
+                        <span>Tambah Template</span>
                     </a>
                 </div>
             </header>
